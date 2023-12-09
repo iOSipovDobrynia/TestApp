689b7ac9cc47e2fdad580c6abf779ecb1612f2ef
iOSipovDobrynia <dobrynia.osipov@gmail.com>
2023-12-09 14:31:48 +0300

    add tests for SoundBar

--- TestApp/SoundBar.swift
+++ TestApp/SoundBar.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 class SoundBar {
-    private var volume = 0
+    private(set) var volume = 0
     
     func setVolume(to value: Int) {
         volume = min(max(value, 0), 100)
--- TestAppTests/SoundBarTests.swift
+++ TestAppTests/SoundBarTests.swift
@@ -6,23 +6,38 @@
 //
 
 import XCTest
+@testable import TestApp
 
 final class SoundBarTests: XCTestCase {
     
     var items: [String] = []
+    
+    var sut: SoundBar!
 
     override func setUp() {
         super.setUp()
-        
+        sut = SoundBar()
     }
 
     override func tearDown() {
-        
+        sut = nil
         super.tearDown()
     }
 
-    func testExample() {
-        
+    func testSetVolumeShouldBeReturnSetedVolume() {
+        let volume = 50
+        sut.setVolume(to: volume)
+        XCTAssertEqual(sut.volume, volume)
+    }
+    
+    func testSetVolumeShouldBeReturnZeroWhenLowestVolume() {
+        sut.setVolume(to: -1)
+        XCTAssertEqual(sut.volume, 0, "Lowest volume should be 0")
+    }
+    
+    func testSetVolumeShouldBeReturnHunderedWhenHighestVolume() {
+        sut.setVolume(to: 1_000)
+        XCTAssertEqual(sut.volume, 100, "Highest volume should be 100")
     }
 
     func testPerformanceExample() {
